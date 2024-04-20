9cbb8239f8e318bcfff0b1ae266c55997329a393
Julio Cesar <jccardosodias0@gmail.com>
2024-04-20 16:42:09 -0300

    criando service e model

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import { Task } from '../models/task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor() { }
+
+  private tasks: Array<Task> = [];
+
+  getTasks(): Array<Task>{
+
+    this.tasks = this.getFromLocalStorage();
+
+    return this.tasks;
+
+    
+  }
+
+  getById(id: number): Task | undefined{
+  
+    const task = this.tasks.find(c => c.id === id);
+  
+  
+    return task;
+  }
+
+  addTask(task: Task): void{
+    this.tasks.push(task);
+    this.saveToLocalStorage();
+  }
+
+  updateTask(){
+
+    this.saveToLocalStorage();
+
+  }
+
+  removeTask(task: Task){
+    const index = this.tasks.indexOf(task);
+    if(index !== -1){
+      //achou
+
+      this.tasks.splice(index, 1);
+
+      this.saveToLocalStorage();
+    }
+    
+  }
+
+  private saveToLocalStorage(){
+
+    const taskJson = JSON.stringify(this.tasks);
+
+    localStorage.setItem('tasks', taskJson);
+  }
+
+  private getFromLocalStorage(): Array<Task>{
+    const tasksJson = localStorage.getItem('tasks');
+
+    if(!tasksJson){
+      //não achou
+
+      return new Array<Task>();
+    }
+
+    return JSON.parse(tasksJson);
+  }
+
+}
+
